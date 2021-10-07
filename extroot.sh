9740be4acc10fe197d1c62d45e816a2529a61140
ittp <61617660+ittp@users.noreply.github.com>
2021-10-07 08:19:24 +0300

    Update extroot.sh

--- extroot.sh
+++ extroot.sh
@@ -1,3 +1,8 @@
 #!/bin/ash
 echo "extroot setup"
 # opkg update && opkg install block-mount kmod-usb-kmod-fs-ext4 kmod-usb-storage
+
+$DIR=/tmp
+$FILE=extroot.sh
+
+#### rm -rf /tmp/extroot.sh && wget https://raw.githubusercontent.com/ittp/openwrt/main/extroot.sh -P /tmp/ && ash /tmp/extroot.sh
