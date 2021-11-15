020eb1cf8a46c008fa479e109d5c2e39d6c56ccd
ittp <61617660+ittp@users.noreply.github.com>
2021-11-15 03:52:16 +0300

    extroot

--- extroot.sh
+++ extroot.sh
@@ -1,8 +1,31 @@
 #!/bin/ash
-echo "extroot setup"
+echo "extroot"
 # opkg update && opkg install block-mount kmod-usb-kmod-fs-ext4 kmod-usb-storage
+etc=/etc
+tmp=/tmp
 
-$DIR=/tmp
-$FILE=extroot.sh
+dev="/dev/sda1"
+
+pkgs="curl nano block-mount kmod-usb-kmod-fs-ext4 kmod-usb-storage"
+
+install() {
+    opkg update
+    opkg install $pkgs
+}
+
+fstab_init {
+    block detect | uci import fstab
+}
+
+prepare_device() {
+    mkfs.ext4 $dev
+}
+
+copy_overlay() {
+    mkdir -p /mnt/$dev
+    mount $dev /mnt/$dev
+    cp -a -f /overlay/. /mnt
+    umount /mnt/$dev
+}
 
 #### rm -rf /tmp/extroot.sh && wget https://raw.githubusercontent.com/ittp/openwrt/main/extroot.sh -P /tmp/ && ash /tmp/extroot.sh
